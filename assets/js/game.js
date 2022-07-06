af58b649841f0d3e1b514d1a2873cb0fefbc69b8
Kit Sterling <kitbanker@gmail.com>
2022-07-05 19:15:04 -0500

    MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,38 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-//fight();
+var enemyName = "Golurk";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+
+    // Player attack
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // Enemy state
+    if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+    } else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // Enemy attack
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    
+    // Player state
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+
+fight();
